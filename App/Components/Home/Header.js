2d917f8848a2beb7d4e5071f1255039aede92899
Alok Agnihotri <96953120+ALAG11@users.noreply.github.com>
2024-03-08 12:55:01 +0530

    Create Header.js

---
+++ App/Components/Home/Header.js
@@ -0,0 +1,30 @@
+import { View, Text, Image } from 'react-native'
+import React from 'react'
+import { useUser } from '@clerk/clerk-expo'
+import { Ionicons } from '@expo/vector-icons';
+
+export default function Header() {
+    const {isLoaded, isSignedIn, user}=useUser();
+    if(!isLoaded || !isSignedIn){
+        return null
+    }
+  return (
+    <View style={{display:'flex', flexDirection:'row',
+     alignItems:'center', justifyContent:'space-between'}}>
+        <View style={{display:'flex', flexDirection:'row', gap:7, alignItems:'center'}}>
+            <Image source={{uri:user.imageUrl}}
+            style={{width:45, height:45, borderRadius:99}}/>
+            <View>
+                <Text>Hello,👋</Text>
+            <Text style={{
+                fontSize:18,
+                fontWeight:'bold'}}>
+            {user.fullName}</Text>
+            </View>
+        </View>
+        <Ionicons name="notifications-outline"
+        size={30} 
+        color="black"/>
+    </View>
+  )
+}
