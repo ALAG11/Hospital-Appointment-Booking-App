0786a67383cab528daa93eff8e9ae15e761b96cc
Alok Agnihotri <96953120+ALAG11@users.noreply.github.com>
2024-03-08 14:48:43 +0530

    Update SearchBar.js

--- App/Components/Home/SearchBar.js
+++ App/Components/Home/SearchBar.js
@@ -1,10 +1,30 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, TextInput } from 'react-native'
+import React, { useState } from 'react'
+import { Ionicons } from '@expo/vector-icons';
+import Colours from '../../../assets/Shared/Colours';
 
-export default function SearchBar() {
+export default function SearchBar({setSearchText}) {
+  const [searchInput, setSearchInput]=useState();
   return (
-    <View style>
-      <Text>SearchBar</Text>
+    <View style={{marginTop:15}}>
+      <View style={{
+        display:'flex',
+        flexDirection:'row',
+        gap:5,
+        alignItems:'center',
+        borderWidth:0.7,
+        borderColor:Colours.GREY,
+        padding:8,
+        borderRadius:8
+
+      }}>
+        <Ionicons name="search-outline" size={24} color={Colours.BLUE} />
+        <TextInput placeholder='Search'
+        onChangeText={(value)=>setSearchInput(value)}
+        onSubmitEditing={()=>setSearchText(searchInput)}
+        style={{width:'100%'}}
+        />
+      </View>
     </View>
   )
 }
