309f180e1af22dd623cbc9e9514b4aa96360c961
Alok Agnihotri <96953120+ALAG11@users.noreply.github.com>
2024-03-08 15:09:03 +0530

    Update App.js

--- App.js
+++ App.js
@@ -6,8 +6,19 @@ import SignInWithOAuth from './App/Components/SignInWithOAuth';
 import Home from './App/Screens/Home';
 import { NavigationContainer } from '@react-navigation/native';
 import TabNavigation from './App/Navigations/TabNavigation';
+import { useFonts } from 'expo-font';
 
-export default function App() {
+export default function App(){
+  const [fontsLoaded] = useFonts({
+    'Orbitron-Medium':require('./assets/fonts/Orbitron-Medium.ttf'),
+    'Orbitron-Regular':require('./assets/fonts/Orbitron-Regular.ttf'),
+    'Orbitron-SemiBold':require('./assets/fonts/Orbitron-SemiBold.ttf'),
+    'Orbitron-Bold':require('./assets/fonts/Orbitron-Bold.ttf'),
+  });
+
+  if(!fontsLoaded){
+    return null;
+  }
   return (
     <ClerkProvider publishableKey={"pk_test_ZGVmaW5pdGUtdGVycmFwaW4tNzguY2xlcmsuYWNjb3VudHMuZGV2JA"}>
     <SafeAreaView style={styles.container}>
