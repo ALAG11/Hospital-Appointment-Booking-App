e7b9e5bd35c9a3a7357f5e1398d7a2b1eb160f47
Alok Agnihotri <96953120+ALAG11@users.noreply.github.com>
2024-03-08 12:56:36 +0530

    Create Home.js

---
+++ App/Screens/Home.js
@@ -0,0 +1,16 @@
+import { View, Text, Button } from 'react-native'
+import React from 'react'
+import { useAuth } from '@clerk/clerk-expo';
+import Header from '../Components/Home/Header';
+import SearchBar from '../Components/Home/SearchBar';
+
+
+export default function Home() {
+    const { isLoaded,signOut } = useAuth();
+  return (
+    <View style={{padding:20, marginTop:20}}>
+        <Header/>
+        <SearchBar/>
+    </View>
+  )
+}
