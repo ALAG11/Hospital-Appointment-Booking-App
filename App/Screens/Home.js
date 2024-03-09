ac2a3433d65d9fdbc731940de394f023475afa2e
Alok Agnihotri <96953120+ALAG11@users.noreply.github.com>
2024-03-09 07:43:08 +0530

    Update Home.js

--- App/Screens/Home.js
+++ App/Screens/Home.js
@@ -3,6 +3,7 @@ import React from 'react'
 import { useAuth } from '@clerk/clerk-expo';
 import Header from '../Components/Home/Header';
 import SearchBar from '../Components/Home/SearchBar';
+import Slider from '../Components/Home/Slider';
 
 
 export default function Home() {
@@ -11,6 +12,9 @@ export default function Home() {
     <View style={{padding:20, marginTop:20}}>
         <Header/>
         <SearchBar setSearchText={(value)=>console.log(value)}/>
+
+        <Slider/>
+
     </View>
   )
 }
