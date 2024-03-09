16dfcbe2abe8b3cfd841f59fbcc16cc68a2b1eb5
Alok Agnihotri <96953120+ALAG11@users.noreply.github.com>
2024-03-09 15:13:35 +0530

    Update Slider.js

--- App/Components/Home/Slider.js
+++ App/Components/Home/Slider.js
@@ -6,23 +6,27 @@ export default function Slider() {
         {
             id:1,
             name:'Slider 1',
-            imageUrl:''
+            imageUrl:'https://img.freepik.com/premium-vector/online-medicine-concept_118813-2776.jpg?w=826'
         },
         {
             id:2,
             name:'Slider 2',
-            imageUrl:''
+            imageUrl:'https://img.freepik.com/free-vector/medical-video-call-consultation-illustration_88138-415.jpg?w=826&t=st=1709977009~exp=1709977609~hmac=87e42a53b7ba7522a02bd0f325f1028e4bd9a86f725cbc612bd5f4090d206dbf'
         }
     ]
   return (
     <View style={{marginTop:10}}>
       <FlatList
         data={sliderList}
+        horizontal={true}
+        showsHorizontalScrollIndicator={false}
         renderItem={({item})=>(
             <Image source={{uri:item.imageUrl}}
                 style={{
                     width:Dimensions.get('screen').width*0.9,
-                    height:170
+                    height:170,
+                    borderRadius:10,
+                    margin:2
                 }}    
             />
         )}
